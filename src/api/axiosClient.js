295e389503b46f6ec9c3d3e75604a692d045f52a
omerkmk03 <omerkumek03@gmail.com>
2025-12-18 20:43:14 +0300

    Update axiosClient baseURL for EC2 deployment

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://3.238.235.255/api",  // EC2 IP
   headers: {
     "Content-Type": "application/json",
   },
